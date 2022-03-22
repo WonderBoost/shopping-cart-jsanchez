8979362d8a63c09e9e47d94e13b6af8ddcd198b5
wonderboost <josegsv28@gmail.com>
2022-03-22 18:00:36 -0400

    cleardb variables

--- config.php
+++ config.php
@@ -1,5 +1,13 @@
+
 <?php
-
-$conn = mysqli_connect('us-cdbr-east-05.cleardb.net','b4419a43f84a15','e234a7ad','heroku_a9d23032b80af2a') or die('connection failed');
-
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
