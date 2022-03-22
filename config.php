91018869243392aa792dc102ff1e1bfceae997fd
wonderboost <josegsv28@gmail.com>
2022-03-22 17:54:34 -0400

    heroku config db

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
+$conn = mysqli_connect('us-cdbr-east-05.cleardb.net','b4419a43f84a15','e234a7ad','heroku_a9d23032b80af2a') or die('connection failed');
 
 ?>
