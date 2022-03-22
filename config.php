25668a6af66e8b6b13ae42c804b1d420a3cfb5c5
wonderboost <josegsv28@gmail.com>
2022-03-22 18:12:10 -0400

    clear db config push

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 
 <?php
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
+$cleardb_url = parse_url(getenv("mysql://b4419a43f84a15:e234a7ad@us-cdbr-east-05.cleardb.net/heroku_a9d23032b80af2a?reconnect=true"));
+$cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
+$cleardb_username = $cleardb_url["b4419a43f84a15"];
+$cleardb_password = $cleardb_url["e234a7ad"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
